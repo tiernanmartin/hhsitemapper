e81fd0e2782ae672ff4fa9385586921a780725e2
Tiernan Martin <tiernan.martin@gmail.com>
2019-05-14 09:51:23 -0700

    add a few funs to utils.R

---
+++ R/utils.R
@@ -0,0 +1,27 @@
+#' @title A set of generic helpful functions
+#' @description General purpose functions to make working in R easier.
+#' @name utils
+#' @import purrr
+NULL
+
+
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+#' Make With Beep
+#'
+#' See \code{drakepkg::\link[drakepkg:make_with_beep]{make_with_beep}} for details.
+#'
+#' @name make_with_beep
+#' @export
+#' @importFrom drakepkg make_with_beep
+NULL
